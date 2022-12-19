179e0b7d4ea1f81d70e2ab90bea0e5bbb5b3a159
SiMONA22 <100865207+simonasablinaite@users.noreply.github.com>
2022-12-19 20:21:10 +0200

    Initial project files structure

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const app = {};
+
+app.init = () => {
+// Pasiruosti pradinius folderius;
+// Pasiruosti pradinius failus;
+// Prisijungimas prie DB (duomenu bazes);
+// Uzkurti pati serveri (musu programa);
+  console.log("Server is running...");
+// Reguliariu procesu paleidimas:
+//   - istrinti nebereikalingus/senus f-lus;
+//   - maziau naudojamu f-lu archivavimas;
+//   - atsinaujinti informacija per/is API (aplication programing interface);
+
+};
+app.init();
+
+export { app };
