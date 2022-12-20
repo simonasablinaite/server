680fcd837337367461d25c3333596cfb0f529c0e
SiMONA22 <100865207+simonasablinaite@users.noreply.github.com>
2022-12-21 00:20:43 +0200

    Created http.server callback

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
+import { server } from "./lib/server.js";
 const app = {};
 
 app.init = () => {
-// Pasiruosti pradinius folderius;
-// Pasiruosti pradinius failus;
-// Prisijungimas prie DB (duomenu bazes);
-// Uzkurti pati serveri (musu programa);
-  console.log("Server is running...");
-// Reguliariu procesu paleidimas:
-//   - istrinti nebereikalingus/senus f-lus;
-//   - maziau naudojamu f-lu archivavimas;
-//   - atsinaujinti informacija per/is API (aplication programing interface);
-
+  // Pasiruosti pradinius folderius;
+  // Pasiruosti pradinius failus;
+  // Prisijungimas prie DB (duomenu bazes);
+  // Uzkurti pati serveri (musu programa);
+  server.init();
+  // Reguliariu procesu paleidimas:
+  //   - istrinti nebereikalingus/senus f-lus;
+  //   - maziau naudojamu f-lu archivavimas;
+  //   - atsinaujinti informacija per/is API (aplication programing interface);
 };
 app.init();
 
